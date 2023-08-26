1425b1e66d307794bab259d4ec3c886d1c0d8424
Geekytoocode <ashishkumarr1108@gmail.com>
2023-08-27 00:02:25 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        Google Keep Clone
+        <HighlightIcon />
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
